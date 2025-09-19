b247c9dace42acf17184a886bdc169064d011aaa
musarratChowdhury <muhitlopez143@gmail.com>
2025-09-19 14:51:53 +0600

    mssql server config added

--- setup_dev_packs.sh
+++ setup_dev_packs.sh
@@ -16,10 +16,20 @@ sudo apt update && sudo apt upgrade -y
 
 ## Install Node.js (LTS)
 echo "Installing Node.js..."
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt install -y nodejs
-node -v
-npm -v
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.19.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.3".
 
 ## Install .NET SDK
 echo "Installing .NET SDK..."
